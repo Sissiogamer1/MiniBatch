fccfee73ae1fe826f7dd5e80677e976a1ed6f24b
Sissiogamer1 <85017864+Sissiogamer1@users.noreply.github.com>
2022-05-08 20:53:40 +0200

    Added MiniBatch Alpha v0.0.4

---
+++ autoexec.bat
@@ -0,0 +1,110 @@
+@echo off
+color 1f
+cd %~dp0
+cd stats
+>nul find "sword1" sword.txt && (
+  set fight=1 >nul
+  set swordtype=Wooden Sword
+) || (
+  >nul find "sword2" sword.txt && (
+  set fight=2 >nul
+  set swordtype=Iron Sword
+) || (
+    >nul find "sword3" sword.txt && (
+  set fight=3 >nul
+  set swordtype=Diamond Sword
+)
+)
+)
+:: Check pick
+>nul find "pick1" pick.txt && (
+  set mine=1 >nul
+  set picktype=Wooden Pick
+) || (
+  >nul find "pick2" pick.txt && (
+  set mine=2 >nul
+  set picktype=Iron Pick
+) || (
+    >nul find "pick3" pick.txt && (
+  set mine=3 >nul
+  set picktype=Diamond Pick
+)
+)
+)
+:: Check axe
+>nul find "axe1" axe.txt && (
+  set chop=1 >nul
+  set axetype=Wooden Axe
+) || (
+  >nul find "axe2" axe.txt && (
+  set chop=2 >nul
+  set axetype=Iron Axe
+) || (
+    >nul find "axe3" axe.txt && (
+  set chop=3 >nul
+  set axetype=Diamond Axe
+)
+)
+)
+>nul find "yes" house.txt && (
+  set house=1 >nul
+) || (
+    set house=0 >nul
+)
+if %house% equ 1 (
+    set safe=1 >nul 
+)
+>nul find "yes" firstrun.txt && (
+  set firstrun=1 >nul
+) || (
+    set firstrun=0 >nul
+)
+if %firstrun% equ 1 (
+    set /a firstrunvar1=0
+    set /a firstrunvar2=0
+    set /a firstrunvar3=0
+    set /a firstrunchoose=0
+    
+)
+if %mine% equ 1 (
+    set /a mineloot=%random% %%26
+)
+if %mine% equ 2 (
+    set /a mineloot=%random% %%51
+)
+if %mine% equ 3 (
+    set /a mineloot=%random% %%101
+)
+if %fight% equ 1 (
+    set /a fightlootzombie=%random% %%9
+    set /a fightlootskeleton=%random% %%4
+    set /a fightlootspider=%random% %%7
+    set /a damage=%random% %%7
+)
+if %fight% equ 2 (
+    set /a fightlootzombie=%random% %%14
+    set /a fightlootskeleton=%random% %%7
+    set /a fightlootspider=%random% %%11
+    set /a damage=%random% %%5
+)
+if %fight% equ 3 (
+    set /a fightlootzombie=%random% %%27
+    set /a fightlootskeleton=%random% %%15
+    set /a fightlootspider=%random% %%26
+    set /a damage=%random% %%3
+)
+if %chop% equ 1 (
+    set /a choplootwood=%random% %%16
+    set /a choplootapple=%random% %%3
+)
+if %chop% equ 2 (
+    set /a choplootwood=%random% %%25
+    set /a choplootapple=%random% %%5
+)
+if %chop% equ 3 (
+    set /a choplootwood=%random% %%33
+    set /a choplootapple=%random% %%9
+)
+cd ..
+set /a health=20
+start minibatch.bat
---
+++ minibatch.bat
@@ -0,0 +1,174 @@
+@echo off
+cd %~dp0
+title MiniBatch Alpha v0.0.4
+echo Welcome in MiniBatch Alpha!
+echo This is still a testing phase, so anything that could be shown in this version could not be on the final release
+echo And anything could change
+timeout /t 2 /nobreak >nul
+echo Press any key to start MiniBatch
+pause >nul
+if %firstrun% equ 1 (
+    echo INTRODUCTION SAMPLE TEXT
+    echo You have a %swordtype%, a %picktype% and a %axetype%.
+    echo Press any key to continue
+    pause >nul
+    cls
+    goto tutorial
+)
+if %firstrun% equ 0 (
+    echo Welcome back!
+    echo Sword: %swordtype%
+    echo Pick: %picktype%
+    echo Axe: %axetype%
+    goto main
+)
+
+:tutorial
+cls
+echo In the alpha phase, you cant fight, chop and mine yourself, but the program does, randomly, depending on your tool
+echo When everything that must be done gets completed, you will be able to chop wood, fight enemies and mine in a cave
+echo For now, you will have a "lobby" where you choose what to do
+timeout /t 5 /nobreak >nul
+echo Press any key to continue
+pause >nul
+goto aftertutorialtext
+
+:aftertutorialtext
+cls
+if %firstrunvar1% equ 1 (
+    set /a firstrunchoose=%firstrunchoose%+1
+)
+cls
+if %firstrunvar2% equ 1 (
+    set /a firstrunchoose=%firstrunchoose%+1
+)
+cls
+if %firstrunvar2% equ 1 (
+    set /a firstrunchoose=%firstrunchoose%+1
+)
+cls
+if %firstrunchoose% equ 3 (
+    echo You finished the tutorial!
+    echo Now go on with your game!
+    goto main
+)
+echo 1. Go mine
+echo 2. Go fight
+echo 3. Go chop
+choice /c 123 /m "What do you want to do?"
+if %errorlevel% equ 1 (
+    goto minetutorial
+)
+if %errorlevel% equ 2 (
+    goto fighttutorial
+)
+if %errorlevel% equ 3 (
+    goto choptutorial
+)
+
+:minetutorial
+cls
+echo Loots will change depending on your tool (and your luck)
+echo Press any key to continue
+pause >nul
+cls
+echo Mining.
+timeout /t 1 /nobreak >nul
+cls
+echo Mining..
+timeout /t 1 /nobreak >nul
+cls
+echo Mining...
+timeout /t 1 /nobreak >nul
+cls
+echo You mined %mineloot% Iron ores with your %picktype%.
+timeout /t 2 /nobreak >nul
+echo Press any key to continue
+pause >nul
+set /a firstrunvar1=1
+goto aftertutorialtext
+
+:fighttutorial
+cls
+echo Monster's loots will change depending on your tool (and your luck)
+echo Press any key to continue
+pause >nul
+cls
+echo Fighting.
+timeout /t 1 /nobreak >nul
+cls
+echo Fighting..
+timeout /t 1 /nobreak >nul
+cls
+echo Fighting...
+timeout /t 1 /nobreak >nul
+cls
+echo You fought some monsters with your %swordtype% and got:
+echo %fightlootskeleton% bones from a skeleton
+echo %fightlootzombie% rotten flesh from a zombie
+echo %fightlootspider% strings from a spider
+timeout /t 1 /nobreak >nul
+echo You also took %damage% damage
+set /a currenthealth=%health%-%damage%
+echo Your health is %currenthealth%
+set /a health=%currenthealth%
+if %health% lss 1 (
+    timeout /t 1 /nobreak >nul
+    cls
+    echo ██  ██   ██████  ██    ██     ██████  ██ ███████ ██████  
+    echo ██  ██  ██    ██ ██    ██     ██   ██ ██ ██      ██   ██ 
+    echo  ████   ██    ██ ██    ██     ██   ██ ██ █████   ██   ██ 
+    echo   ██    ██    ██ ██    ██     ██   ██ ██ ██      ██   ██ 
+    echo   ██     ██████   ██████      ██████  ██ ███████ ██████  
+    echo Sorry for the inconvenience, but this text format is not supported in cmd, this problem will be fixed 
+    timeout /t 3 /nobreak >nul
+    echo In this alpha version, nothing will change if you die and your health will be back to 20
+    set /a health=20
+)
+timeout /t 2 /nobreak >nul
+echo Press any key to continue
+pause >nul
+set /a firstrunvar2=1
+goto aftertutorialtext
+
+:choptutorial
+cls
+echo Wood loots will change depending on your tool (and your luck)
+echo Press any key to continue
+pause >nul
+cls
+echo Chopping.
+timeout /t 1 /nobreak >nul
+cls
+echo Chopping..
+timeout /t 1 /nobreak >nul
+cls
+echo Chopping...
+timeout /t 1 /nobreak >nul
+cls
+echo You chopped %mineloot% Iron ores with your %picktype%.
+timeout /t 2 /nobreak >nul
+echo Press any key to continue
+pause >nul
+set /a firstrunvar3=1
+goto aftertutorialtext
+
+
+
+:: main
+:main
+echo WARNING: The non-tutorial walktrough is not avabile yet in this version, a more complete version will be published as soon as possible
+echo You will still be sent to tutorial actions
+echo 1. Go mine
+echo 2. Go fight
+echo 3. Go chop
+choice /c 123 /m "What do you want to do?"
+if %errorlevel% equ 1 (
+    goto minetutorial
+)
+if %errorlevel% equ 2 (
+    goto fighttutorial
+)
+if %errorlevel% equ 3 (
+    goto choptutorial
+)
